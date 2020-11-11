b56b6e0e065a64eba5e5c3f1466e00c41b02e220
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2020-11-11 13:40:49 -0500

    Add datalad/utils.sh

---
+++ datalad/utils.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function copy_datalad_dataset {
+	while [[ $# -gt 0 ]]
+	do
+		arg="$1"
+		shift # past argument
+		case "${arg}" in
+			--src) SRC="$1"; shift # past value
+			echo "src = [${SRC}]"
+			;;
+			--dest) DEST="$1"; shift # past value
+			echo "dest = [${DEST}]"
+			;;
+			--super-ds) SUPER_DS="$1"; shift # past value
+			echo "super-ds = [${SUPER_DS}]"
+			;;
+			-h | --help | *)
+			if [[ "${arg}" != "-h" ]] && [[ "${arg}" != "--help" ]]
+			then
+				>&2 echo "Unknown option [${arg}]"
+			fi
+			>&2 echo "Options for $(basename "$0") are:"
+			>&2 echo "--src {DIR|DATASET} source dataset directory or name"
+			>&2 echo "--dest DIR ddestination directory"
+			>&2 echo "--super-ds DIR super dataset directory"
+			exit 1
+			;;
+		esac
+	done
+
+	if [[ ! -d "${SRC}" ]]
+	then
+		SRC=${SUPER_DS}/${SRC}
+	fi
+
+	mkdir -p ${DEST}
+
+	! datalad install -s ${SRC}/ ${DEST}/
+	(cd ${DEST}/ && \
+	 link_cache_0fea6a ${SUPER_DS}/.annex-cache && \
+	 git-annex get --fast --from cache-0fea6a || \
+	 git-annex get --fast) || \
+	exit_on_error_code "Failed to copy dataset ${SRC}"
+}
+
+if [[ ! -z "$@" ]]
+then
+	"$@"
+fi
