d5261501a17d36a59377df9e4c8f08ed9c6420cb
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2020-11-11 12:25:35 -0500

    Add jug/jug_utils.sh

---
+++ jug/utils.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function jug_exec {
+	JUG_ARGV=()
+	while [[ $# -gt 0 ]]
+	do
+		arg=$1
+		shift # past argument
+		case ${arg} in
+			--) break ;;
+			*) JUG_ARGV+=("${arg}") ;;
+		esac
+	done
+	# Remove trailing '/' in argv before sending to jug
+	scripts/jug_exec.py "${JUG_ARGV[@]%/}" -- "${@%/}"
+	jug sleep-until "${JUG_ARGV[@]%/}" scripts/jug_exec.py -- "${@%/}"
+}
+
+if [[ ! -z "$@" ]]
+then
+	"$@"
+fi
